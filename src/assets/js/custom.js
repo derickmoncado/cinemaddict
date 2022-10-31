ca4a6e69d9da543a16d2398bc0de17210c6eb2a9
Derick Moncado <derickmoncado@gmail.com>
2022-10-31 15:53:29 -0700

    Working on deleteConfirmModal functionality

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -107,7 +107,7 @@ const fetchEntries = async () => {
 				</div> q
 				<div class="entry__actions">
 					<button type="button" data-entry-id="${entry.id}" id="editEntryBtn"><i class="bi bi-pencil-fill"></i></button>
-					<button type="button" data-entry-id="${entry.id}" id="deleteEntryBtn" onclick="deleteEntry('${entry.id}')"><i class="bi bi-trash-fill"></i></button>
+					<button type="button" data-entry-id="${entry.id}" id="deleteEntryBtn" onclick="deleteEntry('${entry.id}')" data-bs-target="#deleteEntryConfirmModal"><i class="bi bi-trash-fill"></i></button>
 				</div>
 				<div class="entry__streaming-on" style="background-image: url('${entry.entryStreamingOn}')"></div> 
 			</div>
